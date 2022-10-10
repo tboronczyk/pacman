bebd803209b75fb007f599209336c207673474d6
Timothy Boronczyk <tboronczyk@gmail.com>
2022-10-09 22:48:06 -0400

    initial commit

---
+++ build.sh
@@ -0,0 +1,48 @@
+#! /bin/bash
+
+set -e
+
+ORIG="Pac-Man (USA) (Namco).nes"
+ROM="pacman-eo.nes"
+IPS="pacman-eo.ips"
+FLIPS="wine /opt/floating/flips.exe"
+
+echo "GENERATING ROM AND IPS FROM $ORIG..."
+cp "$ORIG" "$ROM"
+
+# thank you Her-Saki
+$FLIPS -a pacman-es.ips "$ORIG" "$ROM"
+
+echo "Updating PRG..."
+dd if=prg/header2.bin of="$ROM" conv=notrunc bs=1 seek=$((0x03C0))
+dd if=prg/character.bin of="$ROM" conv=notrunc bs=1 seek=$((0x06AB))
+dd if=prg/bashful.bin of="$ROM" conv=notrunc bs=1 seek=$((0x06EE))
+dd if=prg/10pts.bin of="$ROM" conv=notrunc bs=1 seek=$((0x0717))
+dd if=prg/50pts.bin of="$ROM" conv=notrunc bs=1 seek=$((0x0722))
+dd if=prg/pause.bin of="$ROM" conv=notrunc bs=1 seek=$((0x0B6B))
+dd if=prg/header1.bin of="$ROM" conv=notrunc bs=1 seek=$((0x34B0))
+dd if=prg/player1.bin of="$ROM" conv=notrunc bs=1 seek=$((0x34C8))
+dd if=prg/player2.bin of="$ROM" conv=notrunc bs=1 seek=$((0x34D4))
+dd if=prg/licensed.bin of="$ROM" conv=notrunc bs=1 seek=$((0x3515))
+dd if=prg/hiscore.bin of="$ROM" conv=notrunc bs=1 seek=$((0x254C))
+dd if=prg/p1.bin of="$ROM" conv=notrunc bs=1 seek=$((0x1114))
+dd if=prg/p1.bin of="$ROM" conv=notrunc bs=1 seek=$((0x255B))
+dd if=prg/p2.bin of="$ROM" conv=notrunc bs=1 seek=$((0x111D))
+dd if=prg/p2.bin of="$ROM" conv=notrunc bs=1 seek=$((0x2561))
+dd if=prg/ready.bin of="$ROM" conv=notrunc bs=1 seek=$((0x3550))
+dd if=prg/gameover.bin of="$ROM" conv=notrunc bs=1 seek=$((0x3580))
+dd if=prg/spacefix.bin of="$ROM" conv=notrunc bs=1 seek=$((0x0EF9))
+dd if=prg/playeronetwo.bin of="$ROM" conv=notrunc bs=1 seek=$((0x3557))
+dd if=prg/playeronetwo.bin of="$ROM" conv=notrunc bs=1 seek=$((0x3588))
+
+echo "Updating CHR..."
+dd if=chr/ux.bin of="$ROM" conv=notrunc bs=1 seek=$((0x4520))
+dd if=chr/titletext1.bin of="$ROM" conv=notrunc bs=1 seek=$((0x4B10))
+dd if=chr/titletext2.bin of="$ROM" conv=notrunc bs=1 seek=$((0x4C10))
+dd if=chr/blank.bin of="$ROM" conv=notrunc bs=1 seek=$((0x4ED0))
+dd if=chr/gametext.bin of="$ROM" conv=notrunc bs=1 seek=$((0x5B10))
+
+echo "Generating patch..."
+$FLIPS -c -i "$ORIG" "$ROM" "$IPS"
+
+echo "Done"
---
+++ pacman-readme.txt
@@ -0,0 +1,15 @@
+Pac Man (NES)
+Traduko al Esperanto v1.0 (22/10/2020)
+BluePhoenix
+
+INFORMOJ PRI ROM
+----------------
+Pac-Man (USA) (Namco).nes
+MD5: CA606BD8A875A396D52735C3BB84FA67
+SHA-1: 92C3361B9E3B28A51FD30E7845C988A6D576EE65
+CRC32: 9E4E9CC2
+
+NOTOJ
+-----
+Ĉi tiu projekto baziĝis ĉefe sur la koduma laboro de Her-Saki en la hispana
+traduko.
